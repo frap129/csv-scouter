00b1258bc7cf1dabbfff77e25a03a7ad6f5af858
Joe Maples <frap129@gmail.com>
2016-02-12 15:47:15 -0500

    Create csv-xlsx.py
    Credits to Nick T at http://superuser.com/questions/301431/how-to-batch-convert-csv-to-xls-xlsx

---
+++ csv-xlsx.py
@@ -0,0 +1,14 @@
+import os
+import glob
+import csv
+import openpyxl # from https://pythonhosted.org/openpyxl/ or PyPI (e.g. via pip)
+
+for csvfile in glob.glob(os.path.join('.', '*.csv')):
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    with open(csvfile, 'rb') as f:
+        reader = csv.reader(f)
+        for r, row in enumerate(reader, start=1):
+            for c, val in enumerate(row, start=1):
+                ws.cell(row=r, column=c).value = val
+    wb.save(csvfile + '.xlsx')
